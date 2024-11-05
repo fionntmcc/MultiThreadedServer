74acf14e6704aae158e0c13c9bbebd5f03a81e37
fionntmcc <127796057+fionntmcc@users.noreply.github.com>
2024-11-05 09:38:06 +0000

    Update ServerThread.java
    sqrt functionality added

--- Server/Server/src/server/ServerThread.java
+++ Server/Server/src/server/ServerThread.java
@@ -31,25 +31,37 @@ public class ServerThread extends Thread {
 			do
 			{
 				// Choose between addition and subtraction
-				sendMessage("Enter 1 for addition and 2 for subtraction: ");
+				sendMessage("Enter 1 for addition, 2 for subtraction and 3 for sqrt: ");
+			
 				msg = (String)in.readObject();
 				choice = msg;
 				
-				sendMessage("Please enter num1");
-				msg = (String)in.readObject();
-				num1 = Integer.parseInt(msg);
-				
-				sendMessage("Please enter num2");
-				msg = (String)in.readObject();
-				num2 = Integer.parseInt(msg);
-				
-				
-				if (choice.equals("1")) {
-					sendMessage("Result is " + (num1 + num2));
+				if (choice.equals("1") || choice.equals("2")) {
+					sendMessage("Please enter num1");
+					msg = (String)in.readObject();
+					num1 = Integer.parseInt(msg);
+					
+					sendMessage("Please enter num2");
+					msg = (String)in.readObject();
+					num2 = Integer.parseInt(msg);
+					
+					
+					if (choice.equals("1")) {
+						sendMessage("Result is " + (num1 + num2));
+					}
+					else if (choice.equals("2")) {
+						sendMessage("Result is " + (num1 - num2));
+					}
 				}
-				else if (choice.equals("2")) {
-					sendMessage("Result is " + (num1 - num2));
+				else if (choice.equals("3")) {
+					sendMessage("Please enter num");
+					msg = (String)in.readObject();
+					num1 = Integer.parseInt(msg);
+					
+					sendMessage("Result is " + (Math.sqrt(num1)));
 				}
+				
+				
 				sendMessage("Enter 1 to repeat");
 				
 				msg = (String)in.readObject();
